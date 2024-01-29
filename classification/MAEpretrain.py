3a7ce3287e5ca0af8afc344a0d984ca42ec23b5d
Ye Yuan <yeyuan@dtu-2svrl12.win.dtu.dk>
2024-01-29 11:11:21 +0100

    MAErunning

--- classification/MAEpretrain.py
+++ classification/MAEpretrain.py
@@ -6,6 +6,8 @@ from pathlib import Path
 
 import yaml
 import torch
+import torch.nn as nn
+
 import torchvision
 import pytorch_lightning as pl
 import pandas as pd
@@ -51,9 +53,13 @@ class MAEpretrainencoder(nn.Module):
             num_heads=self.num_heads,
             hidden_dim=self.hidden_dim,
             mlp_dim=self.mlp_dim,
-            dropout=self.dropout
-        )
+            dropout=self.dropout)
+        
+        
+    
+        
     def patchify(self,img):
+        
         
         # img: (N, 3, H, W)
         # x: (N, L, embed块)
@@ -126,7 +132,7 @@ class MAEpretrain(pl.LightningModule):
             hidden_dim=768,
             mlp_dim=3072,
             dropout=0.1,
-            deco_emb_dim=768,
+            deco_emb_dim=384,
             mask_ratio=0.75):
         super().__init__()
         self.save_hyperparameters()
@@ -147,7 +153,7 @@ class MAEpretrain(pl.LightningModule):
         self.mask_token = nn.Parameter(torch.zeros(1, self.mask_num_block, deco_emb_dim))  #decoder之前加的空白块
         seq_length_encoder =  ((image_size // patch_size) ** 2) + 1
         self.pos_emb_decoder = nn.Parameter(torch.empty(1, seq_length_encoder, deco_emb_dim).normal_(std=0.02))
-        self.deconv_layer = nn.ConvTranspose2d(hidden_dim, 3, kernel_size=patch_size, stride=patch_size, bias=False)
+        self.deconv_layer = nn.ConvTranspose2d(deco_emb_dim, 3, kernel_size=patch_size, stride=patch_size, bias=False)
         self.encoder, self.decoder = self.__build_model()
         
         
@@ -177,7 +183,7 @@ class MAEpretrain(pl.LightningModule):
             patch_size=self.patch_size,
             num_layers=self.num_layers//3,
             num_heads=self.num_heads,
-            hidden_dim=self.deco_emb_dim//2,
+            hidden_dim=self.deco_emb_dim,
             mlp_dim=self.mlp_dim//2,
             dropout=self.dropout
         )
@@ -189,9 +195,13 @@ class MAEpretrain(pl.LightningModule):
     def process_encoder_output(self,x,ids_restore):
         #x(batch,enco块数，encembdim)
         x = self.decoder_emb(x)
-        x = torch.cat((x, self.mask_token), dim=1)
-        x = torch.gather(x, dim=1, index=ids_restore.unsqueeze(-1).repeat(1, 1, x.shape[2]))
-
+        n = x.shape[0]
+        mask_token1 = self.mask_token.expand(n,-1,-1)
+        x = torch.cat((x, mask_token1), dim=1)
+        cls_token1 = x[:,:1,:]
+        x_noclstoken = x[:,1:,:]
+        x_noclstoken = torch.gather(x_noclstoken, dim=1, index=ids_restore.unsqueeze(-1).repeat(1, 1, x.shape[2]))
+        x = torch.cat((cls_token1,x_noclstoken),dim=1)
         return x
         
         
@@ -212,7 +222,7 @@ class MAEpretrain(pl.LightningModule):
     
         return img
     
-     def patchify_img(self, imgs):
+    def patchify_img(self, imgs):
         """
         imgs: (N, 3, H, W)
         x: (N, L, patch_size**2 *3)
@@ -229,13 +239,13 @@ class MAEpretrain(pl.LightningModule):
     
     
     def forward(self, x: torch.Tensor):
-        x,mask,ids_restore = self.encoder(x)
-        
+        x,mask,ids_restore = self.encoder(x) #ids_restore不包含clstoken
+    
         #encoder的输出再进行emb，然后添加空白的块，重排序
         x = self.process_encoder_output(x,ids_restore)
         
         #再加pos_emb
-        x = x +self.pos_emb_decoder
+        x = x + self.pos_emb_decoder
         
         #decode
         x = self.decoder(x)
@@ -257,7 +267,7 @@ class MAEpretrain(pl.LightningModule):
         output,mask = self(images)  #output形状为 (batch_size,3,H,W ) mask（batch_size, L）
         ori_img = self.patchify_img(images)
         pred_img = self.patchify_img(output)  #全变成(N, L, patch_size**2 *3)
-        mask = mask.unsqueeze(2).expand(-1, -1, patch_size**2 * 3)
+        mask = mask.unsqueeze(2).expand(-1, -1, self.patch_size**2 * 3)
         # 只剩下被遮蔽部分
         ori_img = ori_img * mask
         pred_img = pred_img * mask
@@ -288,7 +298,7 @@ class MAEpretrain(pl.LightningModule):
         output,mask = self(images)  #output形状为 (batch_size,3,H,W ) mask（batch_size, L）
         ori_img = self.patchify_img(images)
         pred_img = self.patchify_img(output)  #全变成(N, L, patch_size**2 *3)
-        mask = mask.unsqueeze(2).expand(-1, -1, patch_size**2 * 3)
+        mask = mask.unsqueeze(2).expand(-1, -1, self.patch_size**2 * 3)
         # 只剩下被遮蔽部分
         ori_img = ori_img * mask
         pred_img = pred_img * mask
@@ -438,10 +448,10 @@ def main():
 
     checkpoint_dir = out_dir / "ckpts" 
     checkpointer = pl.callbacks.ModelCheckpoint(dirpath=checkpoint_dir,
-                                                filename='{epoch}-{the_val_loss:.2f}',
+                                                filename='{epoch}-{val_loss_epoch:.2f}',
                                                 save_top_k = 3,
                                                 save_last = True,
-                                                monitor = 'the_val_loss', 
+                                                monitor = 'val_loss_epoch', 
                                                 mode = 'min')
 
     progress_bar_refresh_rate = False
